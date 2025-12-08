26369f9568c4a239619761bc0a60f2f6721f1783
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2025-12-08 16:09:07 +0100

    ajout de la prtie pour ce créer un compte

--- webapps/tpAuthentification/WEB-INF/classes/CreatAccount.java
+++ webapps/tpAuthentification/WEB-INF/classes/CreatAccount.java
@@ -2,22 +2,15 @@ import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 
-/*
-Ecrire une page login.html contenant un formulaire de saisie du login et du mot de passe d’un
-utilisateur et qui appelle une servlet Authent.java qui vérifie dans la base de données si cette
-personne existe ou non et range un token dans la session de l’utilisateur quand il est connu
- */
+@WebServlet("/Creat-Account")
 
-@WebServlet("/Authent")
-public class Authent extends HttpServlet {
-
+public class CreatAccount extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
@@ -31,32 +24,30 @@ public class Authent extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         try (Connection con = DriverManager.getConnection(url, nom, pwd);
-            PreparedStatement pstmt = con.prepareStatement(
-            "SELECT * FROM personne WHERE username = ? AND password = ?;")
-        ) {
+                PreparedStatement pstmt = con.prepareStatement(
+                        "Insert into personne (username, password, role) values ( ?, ?,'util')")) {
             pstmt.setString(1, login);
             pstmt.setString(2, psw);
 
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    HttpSession session = request.getSession();
-                    session.setAttribute("token", "CONNECTE");
-                    session.setAttribute("userLogin", login);
-
-                }
+            try {
+                pstmt.executeUpdate();
+            } catch (Exception e) {
+                out.println("<!doctype html>");
+                out.println("<head><title>servlet Authent Failed</title></head><body><center> ");
+                out.println("<h1>" + e.getLocalizedMessage() + "</h1>");
+                out.println("<h1>" + e.getLocalizedMessage() + "</h1>");
+                out.println("</body></html> ");
             }
 
             con.close();
         } catch (SQLException e) {
-            // pour que ca s'affiche il faut commenter la redirection
+
             out.println("<!doctype html>");
             out.println("<head><title>servlet Authent Failed</title></head><body><center> ");
             out.println("<h1>" + e.getLocalizedMessage() + "</h1>");
             out.println("</body></html> ");
         }
-        // redirection vers Status.java
-        response.sendRedirect("Statut");
 
+        response.sendRedirect("index.html");
     }
-
 }
