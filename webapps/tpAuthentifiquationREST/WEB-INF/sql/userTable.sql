89fb1ebd230d4b2c75b382b9f7f4b9549b5aa136
Hugo Straseele <hugo.straseele.etu@univ-lille.fr>
2026-02-19 14:58:14 +0100

    init du tp

---
+++ webapps/tpAuthentifiquationREST/WEB-INF/sql/userTable.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users(login text, pwd text, ROLE text, token text, nb int);
+INSERT INTO users VALUES ('jean','jean',NULL,NULL,0);
+INSERT INTO users VALUES ('paul','paul',NULL,NULL,0);
